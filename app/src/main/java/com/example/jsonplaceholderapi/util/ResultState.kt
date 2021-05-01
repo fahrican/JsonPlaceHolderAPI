3e8bf8b9592f4af648cc6763f6382a632ac6f8f0
Fahri MacBook <fahri-can@web.de>
2021-05-01 03:26:20 +0200

    Align lines.

--- app/src/main/java/com/example/jsonplaceholderapi/util/ResultState.kt
+++ app/src/main/java/com/example/jsonplaceholderapi/util/ResultState.kt
@@ -1,6 +1,7 @@
 package com.example.jsonplaceholderapi.util
 
 sealed class ResultState<out T : Any> {
+
     data class Success<out T : Any>(val data: T) : ResultState<T>()
     data class Error(val exception: Exception) : ResultState<Nothing>()
     object InProgress : ResultState<Nothing>()
